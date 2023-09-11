cba6f28b341855957f9087d5ec0057d2f4d2034c
Axel <axewah0416@edu.halmstad.se>
2023-09-11 14:36:32 +0200

    Add project files.

---
+++ First Testing/First_Testing.py
@@ -0,0 +1,56 @@
+# print ("Hello, World");
+
+# while True :
+#         print("save me")
+#         break
+# print ("saved")
+# hejsan = 10000;
+
+# print(hejsan);
+# x=2;
+# y=4;
+# x=x/y;
+# y=y/x;
+# print(y);
+# print(3//3);
+
+
+# this language is retarded // don't work'
+#TRUE
+for i in range(0, 100, 1):    
+    if (i+1)%3==0:
+        print("Fizz", end= "");
+    if (i+1)%5==0:
+        print("Buzz", end= "");
+    if (i+1)%5 != 0 and (i+1)%3 != 0:
+        print(i+1, end= "");
+    print();
+
+
+
+
+
+
+
+
+
+incorrInput = True;
+print("Write something followed by ENTER");
+# while incorrInput:
+#        something = input();
+#        if int(something) <= 0:
+#              break
+#        elif int(something) >=0:
+#            break
+#        else:
+#            incorrInput = False;
+something = input();
+print("Your input was " + something);
+somethingElse = input("Now write something else \n")
+print(somethingElse);
+for i in range(100):
+    print("You're dumb", i, end= "\n")
+    pass;
+
+
+
